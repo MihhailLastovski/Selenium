2034ca2a8f29e94014541fb7db67dfccbd866aa0
lasto <lasto@ZXC>
2023-10-01 20:10:39 +0300

    project created

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Set the path to the ChromeDriver executable
+        var driverPath = "C:/Users/lasto/Downloads/chromedriver_win32";
+
+        // Create a ChromeDriver instance
+        IWebDriver driver = new ChromeDriver(driverPath);
+
+        // Navigate to a website (e.g., Google)
+        driver.Navigate().GoToUrl("https://www.google.com");
+
+        // Find the search input element and type a query
+        IWebElement searchBox = driver.FindElement(By.Name("q"));
+        searchBox.SendKeys("Your search query");
+
+        // Submit the search
+        searchBox.Submit();
+
+        // Wait for a few seconds (you may need to add proper waits)
+        System.Threading.Thread.Sleep(5000);
+
+        // Close the browser
+        driver.Quit();
+    }
+}
